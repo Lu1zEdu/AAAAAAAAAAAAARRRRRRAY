ada24bc6419c6c4abb0b4df2497456b35495aabd
Lu1zEdu <le6269199@gmail.com>
2024-08-07 08:30:01 -0300

    CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC

---
+++ Js/Script.js
@@ -0,0 +1,10 @@
+
+let fruta = [
+    "abacaxi", "maca", "banana", "laranja" , "melancia" , "abacaxi" , "Zimbro" , "abacate" , "Lichia"
+];
+
+let novasFrutas = fruta.map(function(ItemDoArray){
+    return fruta.toUpperCase();
+});
+
+console.log(novasFrutas);
